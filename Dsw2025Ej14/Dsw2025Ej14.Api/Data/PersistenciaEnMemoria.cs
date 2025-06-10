16ddab14e84336bc4f9bf1e333d2fb723c844301
maxivega0 <maximilianovega600@gmail.com>
2025-06-10 15:22:08 -0300

    Estructura y product

---
+++ Dsw2025Ej14/Dsw2025Ej14.Api/Data/PersistenciaEnMemoria.cs
@@ -0,0 +1,33 @@
+﻿using Dsw2025Ej14.Api.Domain;
+using System.Text.Json;
+
+namespace Dsw2025Ej14.Api.Data
+{
+    public class _PersistenciaEnMemoria_
+    {
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Dsw2025Ej14/Dsw2025Ej14.Api/Domain/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace Dsw2025Ej14.Api.Domain
+{
+    public class Product
+    {
+        public string SKU { get; set; }
+        public string Name { get; set; }
+        public decimal CurrentUnitPrice { get; set; }
+        public bool IsActive { get; set; }
+
+        public Product(string sku, string name, decimal currentUnitPrice, bool isActive)
+        {
+            SKU = sku;
+            Name = name;
+            CurrentUnitPrice = currentUnitPrice;
+            IsActive = isActive;
+        }
+    }
+}
